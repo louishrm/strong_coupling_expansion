1fffe24569ce52651f04fe0aea6ebf0c32479623
Michel Ferrero <mferrero@cpht.polytechnique.fr>
2025-11-27 11:47:10 +0100

    Add root transform

--- c++/sc_expansion/dimer_order_4.hpp
+++ c++/sc_expansion/dimer_order_4.hpp
@@ -4,6 +4,17 @@
 
 namespace sc_expansion {
 
+std::function<std::vector<double>(std::vector<double>)> make_x_to_tau(double beta) {
+
+  return [beta](std::vector<double> xs) -> std::vector<double> {
+    int n = xs.size();
+    std::vector<double> taus(xs.size());
+    taus[0] = beta * std::pow(xs[0], 1.0 / (double)n);
+    for (int i = 1; i < xs.size(); i++) { taus[i] = taus[i - 1] * std::pow(xs[i], 1.0 / ((double)(n - i))); }
+    return taus;
+  };
+}
+
 class order4 {
 
   public:
@@ -22,10 +33,11 @@ class order4 {
     diagrams({Diagram(D4a), Diagram(D4b), Diagram(D4c)}),
     beta(beta_) {}
 
-  double compute_sum_diagrams(std::vector<double> taus) {
+  double compute_sum_diagrams(std::vector<double> x) {
 
     double diagram_sum = 0.0;
     for (auto const &diagram : diagrams) {
+      auto taus = make_x_to_tau(beta)(x);
       double val = diagram.evaluate_at_taus(ad, beta, taus);
       diagram_sum += val;
     }
